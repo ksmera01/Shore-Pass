3cb585c7fc8841e72dd125aa13601967b3780a8b
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-08-02 12:22:01 -0400

    updated footer component

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,19 +1,10 @@
 import React, { useState, useContext } from 'react';
-// import { TransactionContext } from '../context/TransactionContext';
-// import { useHistory } from "react-router-dom";
-// import Button from '@material-ui/core/Button';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-// import Location from '../components/Location';
 
 const useStyles = makeStyles((theme) => ({
     link: {
